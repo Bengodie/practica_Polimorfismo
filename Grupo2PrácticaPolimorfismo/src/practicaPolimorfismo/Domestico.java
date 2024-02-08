eb6a8424be82f8da259e974e927164632f5d2c93
IndiraRamirez <145410298+IndiraRamirez@users.noreply.github.com>
2024-02-07 23:25:53 -0600

    Crear clases

---
+++ Grupo2PrácticaPolimorfismo/src/practicaPolimorfismo/Domestico.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package practicaPolimorfismo;
+
+/**
+ *
+ * @author indir
+ */
+public interface Domestico {
+    public abstract int asignarPeso();
+     public abstract void  ladrar();
+      public abstract void perseguir();
+}
